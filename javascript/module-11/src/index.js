fc3490d6352f47c3b00505df63eb558a98d25e39
Vladimir Viazankin <vyazan41k@gmail.com>
2020-10-08 21:57:06 +0300

    module-11

--- javascript/module-11/src/index.js
+++ javascript/module-11/src/index.js
@@ -1,6 +1,6 @@
 import './styles.css';
 import './js/colors';
 import './js/timer';
-// import './js/promise/task1';
-// import './js/promise/task2';
+import './js/promise/task1';
+import './js/promise/task2';
 import './js/promise/task3';
--- javascript/module-11/src/js/promise/task3.js
+++ javascript/module-11/src/js/promise/task3.js
@@ -9,14 +9,15 @@ const makeTransaction = transaction => {
     setTimeout(() => {
       const canProcess = Math.random() > 0.3;
       if (canProcess) {
-        resolve(transaction.id, delay);
+        resolve({ id: transaction.id, time: delay });
+      } else {
+        reject(transaction.id);
       }
-      reject(transaction.id);
     }, delay);
   });
 };
 
-const logSuccess = (id, time) => {
+const logSuccess = ({ id, time }) => {
   console.log(`Transaction ${id} processed in ${time}ms`);
 };
 
